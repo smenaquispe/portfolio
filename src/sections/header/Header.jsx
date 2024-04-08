3c1949ae65996a4f52f4e03d9115d1c2482b7274
smena <sergiosantosmenaq@gmail.com>
2024-04-07 21:27:42 -0500

    more clean header

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -6,16 +6,14 @@ export function Header() {
   return (
     <>
       <BigTitle>SERGIO MENA</BigTitle>
-      <div className="felx w-full flex-col justify-center px-[12rem]">
-        <div className="my-10 w-1/2">
-          <Card>
-            <Paragraph color={'dark'}>
-              Hola soy <span className="text-rose">Sergio Mena</span>, desarrollador de software con un año de
-              experiencia profesional y más de 3 años como programador autodidacta. Me apasiona la tecnología y la
-              programación, me encanta aprender cosas nuevas y enfrentar desafíos.
-            </Paragraph>
-          </Card>
-        </div>
+      <div className="my-10 flex w-full justify-center gap-8 px-[12rem]">
+        <Card>
+          <Paragraph color={'dark'}>
+            Hola soy <span className="text-rose">Sergio Mena</span>, desarrollador de software con un año de experiencia
+            profesional y más de 3 años como programador autodidacta. Me apasiona la tecnología y la programación, me
+            encanta aprender cosas nuevas y enfrentar desafíos.
+          </Paragraph>
+        </Card>
       </div>
     </>
   );
--- src/shared/components/Card.jsx
+++ src/shared/components/Card.jsx
@@ -1,4 +1,4 @@
 // eslint-disable-next-line react/prop-types
 export function Card({ children }) {
-  return <article className="rounded-lg border-[1px] border-rose p-4">{children}</article>;
+  return <article className="rounded-lg p-4">{children}</article>;
 }
